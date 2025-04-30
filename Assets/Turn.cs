c90ff0f06ce134f04abb01e5086b5d6c2fff0ac6
Kevin S. Tran <tanksense@gmail.com>
2025-04-30 18:48:30 +1000

    Week 1

---
+++ Assets/Turn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    public float speed = 180;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate( 0,
+                          Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+                          0
+                        );
+    }
+}
