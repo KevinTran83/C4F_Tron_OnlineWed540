0af558adb73d5eb514599aa9b1912db003f87c01
Kevin S. Tran <tanksense@gmail.com>
2025-05-14 18:42:55 +1000

    Week 3

--- Assets/Line.cs
+++ Assets/Line.cs
@@ -6,22 +6,44 @@ public class Line : MonoBehaviour
 {
     public List<Vector3> positions;
     private LineRenderer rend;
+    public float interval = 0.2f;
+    public int length = 50;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<LineRenderer>();
         rend.positionCount = 0;
+        Invoke("RecordPosition", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        RecordPosition();
+        PlayerCheck();
     }
 
     void RecordPosition()
     {
         positions.Add(transform.position);
+
+        if (positions.Count > length) positions.RemoveAt(0);
+
+        rend.positionCount = positions.Count;
+        rend.SetPositions(positions.ToArray());
+
+        Invoke("RecordPosition", interval);
+    }
+
+    void PlayerCheck()
+    {
+        for (int i = 0; i < positions.Count - 1 - 4; i++)
+        {
+            Vector3 vect = positions[i + 1] - positions[i];
+
+            RaycastHit hit;
+            if (Physics.Raycast(positions[i], vect, out hit, vect.magnitude))
+                Destroy(hit.transform.gameObject);
+        }
     }
 }
